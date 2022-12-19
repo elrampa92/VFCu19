cf5ccbb800169ee40e64f6ec6a040031147dcf7c
elrampa92 <119736137+elrampa92@users.noreply.github.com>
2022-12-19 17:03:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("hello")
+st.write("provissima!")
+
+url = "https://raw.githubusercontent.com/elrampa92/VFCu19_Dashboard/main/MINUTAGGI/MINUTAGGI.csv" # Make sure the url is the raw version of the file on GitHub
+download = requests.get(url).content
+
+
+df_minutaggi = pd.read_csv(url)
+
+st.write(df_minutaggi)
