2f03a7e2ea3b9f3fbdfdaa83a35605a77484956b
elrampa92 <119736137+elrampa92@users.noreply.github.com>
2023-01-26 23:08:54 +0100

    Update 02_Presenze.py

--- pages/02_Presenze.py
+++ pages/02_Presenze.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import requests
 import altair as alt
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from pprint import pprint
 
 st.set_page_config(page_title="VFC u19 Dashboard", layout="wide")
