cb05caacdfdd112629a14dc420464e293db51931
elrampa92 <119736137+elrampa92@users.noreply.github.com>
2023-02-06 20:23:32 +0100

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 import requests
 import altair as alt
-import matplotlib.pyplot as plt
-from pprint import pprint
+#import matplotlib.pyplot as plt
+#from pprint import pprint
 
 st.set_page_config(page_title="VFC u19 Dashboard", layout="wide")
 st.title("Homepage")
