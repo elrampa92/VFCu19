22f890dffb57d1b6d6fa9a0af3025d7df7f4b1e4
elrampa92 <119736137+elrampa92@users.noreply.github.com>
2023-03-02 22:42:17 +0100

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import requests
 import altair as alt
-#import matplotlib.pyplot as plt
-#from pprint import pprint
+import matplotlib.pyplot as plt
+from pprint import pprint
+
 
 st.set_page_config(page_title="VFC u19 Dashboard", layout="wide")
 st.title("Homepage")
@@ -13,18 +15,18 @@ st.sidebar.success("Seleziona una pagina")
 url = "https://raw.githubusercontent.com/elrampa92/VFCu19_Dashboard/main/MINUTAGGI/MINUTAGGI.csv" # Make sure the url is the raw version of the file on GitHub
 download = requests.get(url).content
 
-url_min = "https://raw.githubusercontent.com/elrampa92/VFCu19_Dashboard/main/MINUTAGGI/MINUTAGGI.xlsx" # Make sure the url is the raw version of the file on GitHub
-df_min = pd.read_excel(url_min, usecols = "A,B,AN")
-df_min_status = pd.read_excel(url_min, usecols = "A,B,AL:AN")
-df_status = pd.read_excel(url_min, usecols = "A,B,AC:AJ")
+url_min = "https://raw.githubusercontent.com/elrampa92/VFCu19_Dashboard/main/DATABASE/VENEZIA.xlsx" # Make sure the url is the raw version of the file on GitHub
+df_min = pd.read_excel(url_min, sheet_name='minuti', usecols = "A,C,D:V") #dataframe con minutaggi pp
 
+df_min_ts = pd.read_excel(url_min, sheet_name='minuti', usecols = "A,C,AH:AJ") #dataframe con minuti giocati + titolare e subentrato
 
+df_status = pd.read_excel(url_min, sheet_name='stati', usecols = "A,C,AH:AO") #dataframe con stati pp
 
 col1, col2, col3 = st.columns(3)
 
 with col1:
    st.subheader("Top 10 Minuti giocati")
-   tmp_df = df_min
+   tmp_df = df_min_ts.drop(columns = ['TITOLARE','SUBENTRATO'])
    tmp_df = tmp_df.sort_values(by = ['MINUTI TOTALI'], ascending=False)
    tmp_df = tmp_df.head(10)
    st.dataframe(tmp_df, use_container_width=True)
