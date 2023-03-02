e0a87396a8ee3e196518d50d2a163454efa0a779
elrampa92 <119736137+elrampa92@users.noreply.github.com>
2023-03-02 22:41:01 +0100

    Add files via upload

---
+++ pages/03_Statistiche.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+import altair as alt
+from pprint import pprint
+
+st.set_page_config(page_title="VFC u19 Dashboard", layout="wide")
+st.title("Dati GPS")
