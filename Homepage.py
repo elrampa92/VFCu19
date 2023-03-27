1bd65e03d18cff72632da7b3f479813cc3a40ba5
elrampa92 <119736137+elrampa92@users.noreply.github.com>
2023-03-27 23:18:42 +0200

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -12,8 +12,8 @@ st.title("Homepage")
 st.sidebar.success("Seleziona una pagina")
 
 ### LEGGO DATAFRAME CON MINUTAGGI
-url = "https://raw.githubusercontent.com/elrampa92/VFCu19_Dashboard/main/MINUTAGGI/MINUTAGGI.csv" # Make sure the url is the raw version of the file on GitHub
-download = requests.get(url).content
+#url = "https://raw.githubusercontent.com/elrampa92/VFCu19_Dashboard/main/MINUTAGGI/MINUTAGGI.csv" # Make sure the url is the raw version of the file on GitHub
+#download = requests.get(url).content
 
 url_min = "https://raw.githubusercontent.com/elrampa92/VFCu19_Dashboard/main/DATABASE/VENEZIA.xlsx" # Make sure the url is the raw version of the file on GitHub
 df_min = pd.read_excel(url_min, sheet_name='minuti', usecols = "A,C,D:V") #dataframe con minutaggi pp
@@ -22,18 +22,23 @@ df_min_ts = pd.read_excel(url_min, sheet_name='minuti', usecols = "A,C,AH:AJ") #
 
 df_status = pd.read_excel(url_min, sheet_name='stati', usecols = "A,C,AH:AO") #dataframe con stati pp
 
+df_gol_fatti = pd.read_excel(url_min, sheet_name='gol', usecols = "A,C,AH") #dataframe con gol fatti
+
 col1, col2 = st.columns(2)
 
 with col1:
-   st.subheader("Top 10 per minuti giocati")
+   st.subheader("Top 5 per minuti giocati")
    tmp_df = df_min_ts.drop(columns = ['TITOLARE','SUBENTRATO'])
    tmp_df = tmp_df.sort_values(by = ['MINUTI TOTALI'], ascending=False)
-   tmp_df = tmp_df.head(10)
+   tmp_df = tmp_df.head(5)
    st.dataframe(tmp_df, use_container_width=True)
    #st.write(tmp_df.to_html(escape=False, index=False), unsafe_allow_html=True)
 
 with col2:
    st.subheader("Top 5 marcatori")
+   tmp_df_gf = tmp_df_gf.sort_values(by = ['GOL FATTI'], ascending=False)
+   tmp_df_gf = tmp_df_gf.head(5)
+   st.dataframe(tmp_df, use_container_width=True)
 
 
 
