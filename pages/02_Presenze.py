fdf7983de2c1733f03aa77dd4cbd39bd85957e95
elrampa92 <119736137+elrampa92@users.noreply.github.com>
2023-04-18 22:37:30 +0200

    Update 02_Presenze.py

--- pages/02_Presenze.py
+++ pages/02_Presenze.py
@@ -114,7 +114,26 @@ with Sanzioni:
   provadf = pd.DataFrame(data)
                                        
   st.dataframe(provadf ,use_container_width=False)
+  
+  st.bar_chart(provadf)
 
+  # Convert wide-form data to long-form
+  # See: https://altair-viz.github.io/user_guide/data.html#long-form-vs-wide-form-data
+  data = pd.melt(provadf.reset_index(), id_vars=["index"])
+
+  # Horizontal stacked bar chart
+  chart = (
+      alt.Chart(data)
+      .mark_bar()
+      .encode(
+          x=alt.X("value", type="quantitative", title=""),
+          y=alt.Y("index", type="nominal", title=""),
+          color=alt.Color("variable", type="nominal", title=""),
+          order=alt.Order("variable", sort="descending"),
+      )
+  )
+
+  st.altair_chart(chart, use_container_width=True)
                                        
 
 
