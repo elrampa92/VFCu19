e5005fffa6bdbd58e46199c7fd580847dd728082
elrampa92 <119736137+elrampa92@users.noreply.github.com>
2023-04-22 13:38:00 +0200

    Update 03_Allenamenti.py

--- pages/03_Allenamenti.py
+++ pages/03_Allenamenti.py
@@ -1,1 +1,24 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+import altair as alt
+import matplotlib.pyplot as plt
+from pprint import pprint
 
+st.set_page_config(page_title="VFC u19 Dashboard", layout="wide")
+st.title("Riepilogo allenamenti 22/23")
+st.header("Campionato Primavera 2 A")
+
+
+Settimanali, Mensili, Totali = st.tabs(["Settimanali", "Mensili", "Totali"])
+
+with Settimanali:
+  
+  
+  
+with Mensili:
+  
+  
+  
+with Totali:
