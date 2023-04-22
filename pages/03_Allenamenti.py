84bca23f6d96a9ba9f9d01342b7fdfee69c83525
elrampa92 <119736137+elrampa92@users.noreply.github.com>
2023-04-22 13:40:37 +0200

    Update 03_Allenamenti.py

--- pages/03_Allenamenti.py
+++ pages/03_Allenamenti.py
@@ -8,7 +8,7 @@ from pprint import pprint
 
 st.set_page_config(page_title="VFC u19 Dashboard", layout="wide")
 st.title("Riepilogo allenamenti 22/23")
-st.header("Campionato Primavera 2 A")
+#st.header("Campionato Primavera 2 A")
 
 
 Settimanali, Mensili, Totali = st.tabs(["Settimanali", "Mensili", "Totali"])
